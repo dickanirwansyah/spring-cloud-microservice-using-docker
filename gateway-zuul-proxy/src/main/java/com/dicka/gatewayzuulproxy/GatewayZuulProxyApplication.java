969d118e8c488e45272eb515876f9e59fd755d4f
dickanirwansyah <dicka.nirwansyah@fusi24.com>
2018-12-23 01:15:01 +0700

    api gateway

--- gateway-zuul-proxy/src/main/java/com/dicka/gatewayzuulproxy/GatewayZuulProxyApplication.java
+++ gateway-zuul-proxy/src/main/java/com/dicka/gatewayzuulproxy/GatewayZuulProxyApplication.java
@@ -2,8 +2,12 @@ package com.dicka.gatewayzuulproxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
 public class GatewayZuulProxyApplication {
 
 	public static void main(String[] args) {
