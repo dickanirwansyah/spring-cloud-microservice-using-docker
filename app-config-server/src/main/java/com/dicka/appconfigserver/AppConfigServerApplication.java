519bd787fc83b51ddbc641697b43285555a61c1e
dickanirwansyah <dicka.nirwansyah@fusi24.com>
2018-12-22 22:42:18 +0700

    config server

---
+++ app-config-server/src/main/java/com/dicka/appconfigserver/AppConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.dicka.appconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppConfigServerApplication.class, args);
+	}
+
+}
+
---
+++ app-config-server/src/test/java/com/dicka/appconfigserver/AppConfigServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dicka.appconfigserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
